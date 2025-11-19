6b064afa08e85a0a884e1e0aed98991b6c5e29f7
Alexey Semenov <midhey@yandex.ru>
2025-11-19 21:12:17 +0300

    chore: add settings to nuxt.config

--- frontend/nuxt.config.ts
+++ frontend/nuxt.config.ts
@@ -1,6 +1,17 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  compatibilityDate: '2025-07-15',
+  compatibilityDate: "2025-07-15",
   devtools: { enabled: true },
-  modules: ['@pinia/nuxt']
-})
+  modules: ["@pinia/nuxt"],
+  css: ["~/assets/scss/main.scss"],
+  runtimeConfig: {
+    public: {
+      apiBase:
+        process.env.NUXT_PUBLIC_API_BASE || "http://localhost:8080/api/v1",
+    },
+  },
+  typescript: {
+    strict: true,
+  },
+});
+
