5de59c191fb0be02d42a48ec7e72c43f909b1eb7
Alexey Semenov <midhey@yandex.ru>
2025-11-24 17:30:33 +0300

    feat(layouts): add default layout

---
+++ frontend/layouts/default.vue
@@ -0,0 +1,14 @@
+<template>
+  <div class="app wrapper">
+    <AppHeader />
+      <main class="main">
+        <ClientOnly>
+          <NuxtPage/>
+        </ClientOnly>
+      </main>
+  </div>
+</template>
+
+<script setup lang="ts">
+import AppHeader from "~/components/layout/AppHeader.vue";
+</script>
