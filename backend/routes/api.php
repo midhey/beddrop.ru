c973fc0d76bfe8e1e7c0787835a5aa51348ae006
Alexey Semenov <midhey@yandex.ru>
2025-11-25 18:27:31 +0300

    feat(restaurant): add route to api

--- backend/routes/api.php
+++ backend/routes/api.php
@@ -7,6 +7,8 @@ use App\Http\Controllers\Auth\RegisterController;
 use App\Http\Controllers\MediaController;
 use App\Http\Controllers\Profile\PasswordController;
 use App\Http\Controllers\Profile\ProfileController;
+use App\Http\Controllers\RestaurantController;
+use App\Http\Controllers\RestaurantStaffController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/ping', fn () => ['message' => 'pong']);
@@ -28,6 +30,22 @@ Route::prefix('/v1')->group(function () {
         Route::put('me', [ProfileController::class, 'update']);
         Route::put('password', PasswordController::class);
     } );
+
+    Route::prefix('/restaurants')->group(function () {
+        Route::get('/', [RestaurantController::class, 'index']);
+        Route::get('/{restaurant:slug}', [RestaurantController::class, 'show']);
+
+        Route::middleware('auth:api')->group(function () {
+            Route::post('/', [RestaurantController::class, 'store']);
+            Route::put('/{restaurant}', [RestaurantController::class, 'update']);
+            Route::delete('/{restaurant}', [RestaurantController::class, 'destroy']);
+
+            Route::get('/{restaurant}/users', [RestaurantStaffController::class, 'index']);
+            Route::post('/{restaurant}/users', [RestaurantStaffController::class, 'store']);
+            Route::put('/{restaurant}/users/{user}', [RestaurantStaffController::class, 'update']);
+            Route::delete('/{restaurant}/users/{user}', [RestaurantStaffController::class, 'destroy']);
+        });
+    });
 
     Route::middleware('auth:api')->group(function () {
         Route::post('/media', [MediaController::class, 'store']);
