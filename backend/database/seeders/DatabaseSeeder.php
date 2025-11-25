97141ce2e5ad8f76579e678bff89e291131230c2
Alexey Semenov <midhey@yandex.ru>
2025-11-25 18:27:24 +0300

    feat(restaurant): add seeder

--- backend/database/seeders/DatabaseSeeder.php
+++ backend/database/seeders/DatabaseSeeder.php
@@ -15,7 +15,9 @@ class DatabaseSeeder extends Seeder
      */
     public function run(): void
     {
-
-
+        $this->call([
+            UserSeeder::class,
+            RestaurantSeeder::class,
+        ]);
     }
 }
